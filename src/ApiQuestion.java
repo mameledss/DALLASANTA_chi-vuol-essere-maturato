dbb967cd2ad0acd25286d324f449c27a33e8d305
mameledss <10934160@itisrossi.vi.it>
2025-09-24 11:32:23 +0200

    Aggiunta ApiQuestion.java e ApiResponse.java

---
+++ src/ApiQuestion.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class ApiQuestion {
+    public String category;
+    public String type;
+    public String difficulty;
+    public String question;
+    public String correct_answer;
+    public List<String> incorrect_answers;
+}
---
+++ src/ApiResponse.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public class ApiResponse {
+    public int response_code;
+    public List<ApiQuestion> results;
+}
